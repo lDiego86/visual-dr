4a057a6543d7b82d2ead268b5b46e86938f6a857
Luis Diego Rodriguez Abarca <ldrodriguez@ccss.sa.cr>
2026-03-25 13:25:06 -0600

    fix ruta correcta public

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import AboutBg from "../img/about/about.png"; // ✅ corregido
+import AboutBg from "../img/about/about.png"; 
 
 export default function AboutSection() {
   return (
