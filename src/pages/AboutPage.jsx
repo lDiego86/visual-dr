0faf59f16cf112d6556317c6cf88a7c772c5fb8d
Luis Diego Rodriguez Abarca <ldrodriguez@ccss.sa.cr>
2026-03-25 13:08:32 -0600

    fix imagen about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import AboutBg from "../img/about/about.png"; // ✅ corregido
+import AboutBg from "../img/about/About.png"; // ✅ corregido
 
 export default function AboutSection() {
   return (
